2ad66ea6bcfa71ddfd924f0d1c5e47317330e5bc
NoNameGivenCoder <chaytontimedis@gmail.com>
2023-12-15 21:54:40 -0700

    EJS rendering communities

--- routes/titles.js
+++ routes/titles.js
@@ -2,10 +2,20 @@ const express = require('express');
 const route = express.Router();
 const xmlbuilder = require('xmlbuilder');
 const moment = require('moment');
-const Base64 = require('../static/js/base64')
+const Base64 = require('../static/js/base64');
 
-route.get('/show', (req, res) => {
-    res.render('titles.ejs');
+const util = require('util');
+
+const con = require('../database_con');
+const query = util.promisify(con.query).bind(con);
+
+route.get('/show', async (req, res) => {
+
+    var communities = (await query("SELECT * FROM communities ORDER BY create_time DESC"));
+
+    res.render('titles.ejs', {
+        communities : communities
+    });
 })
 
 route.get('/activity_feed', (req, res) => {
