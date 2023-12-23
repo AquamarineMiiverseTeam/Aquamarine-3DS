7385e5cf4ad522ad9032d655b573a2d47c6243f8
NoNameGivenCoder <chaytontimedis@gmail.com>
2023-12-22 22:08:57 -0700

    added post displaying to communities page

--- routes/ui/communities.js
+++ routes/ui/communities.js
@@ -10,16 +10,25 @@ const query = util.promisify(con.query).bind(con);
 
 route.get('/:id', async(req, res) => {
 
-    const community = (await query("SELECT * FROM communities WHERE id=?", req.params.id))[0]
+    const community = (await query("SELECT * FROM communities WHERE id=?", req.params.id))[0];
 
     //TODO: add error page
     if (!community) {res.sendStatus(404);}
     
-    const posts = await query("SELECT * FROM posts WHERE community_id=?", req.params.id)
+    const posts = await query("SELECT * FROM posts WHERE community_id=?", req.params.id);
+
+    //Adding account data to each post
+    for (let i = 0; i < posts.length; i++) {
+        const account = (await query("SELECT * FROM accounts WHERE id=?", posts[i].account_id))[0];
+        
+        posts[i].mii_image = `http://mii-images.account.nintendo.net/${account.mii_hash}_normal_face.png`;
+        posts[i].mii_name = account.mii_name;
+    }
 
     res.render('communities.ejs', {
         community : community,
-        posts : posts
+        posts : posts,
+        moment : moment
     });
 })
 
